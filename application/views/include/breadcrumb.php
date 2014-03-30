e1ee5a74d9c057205a488de700ddfe3862bae98b
stickstick <stick@masterofbits.com>
2014-03-30 19:42:51 -0400

    retab view files

--- application/views/include/breadcrumb.php
+++ application/views/include/breadcrumb.php
@@ -1,6 +1,6 @@
     <ul id="breadcrumb">
-		<?foreach($this->breadcrumb as $b) {?>
-			<li><a href="<?=base_url().$b['url'];?>"><?=$b['name'];?></a></li>
-		<?}?>
+        <?foreach($this->breadcrumb as $b) {?>
+            <li><a href="<?=base_url().$b['url'];?>"><?=$b['name'];?></a></li>
+        <?}?>
     </ul>
-	<br /><br />
+    <br /><br />
